6542a59bf8d0aef5da0c9999b22fc55aaa8fa5a0
Frederik Dudzik <frederik@dudzik.co>
2016-04-27 20:15:27 +0200

    add simple_task to tap

---
+++ simple_task.rb
@@ -0,0 +1,23 @@
+class SimpleTask < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/doodzik/simple_task/archive/0.0.1.tar.gz"
+  version "0.0.1"
+  sha256 "28a0d4eaa487076af4881c568662464413de6fd22edb0ed8b413f1ffa48a59b2"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+
+    bin.install "simple_task_list" 
+    bin.install "simple_task"
+    bin.install "_simple_task_complete"
+    bin.install "_simple_task_list_completen"
+  end
+
+  test do
+    system "false"
+  end
+end
