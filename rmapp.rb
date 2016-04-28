48ac8ede48e25159a7c56226f16c9a98c3fa2713
Frederik Dudzik <frederik@dudzik.co>
2016-04-28 10:07:23 +0200

    upgrade rmapp to version 0.0.2

--- rmapp.rb
+++ rmapp.rb
@@ -1,11 +1,12 @@
 class Rmapp < Formula
   desc "removes app and all its data from osx"
   homepage "https://github.com/doodzik/rmapp"
-  url "https://github.com/doodzik/rmapp/archive/0.0.1.tar.gz"
-  version "0.0.1"
-  sha256 "da45b74c527521f4b03c49afa7cb5a5bc5c74f3c040c07cb8e7087bdd24a9d02"
+  url "https://github.com/doodzik/rmapp/archive/0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "c5a4850f3f4166c9df750644188711333d89e32478545207f99e2babfb76e4b2"
 
   def install
     bin.install "rmapp"
+    bin.install "rmapp_app_list"
   end
 end
