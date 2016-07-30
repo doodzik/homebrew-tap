5bbf6dcd87f177a105df616467a682b5e1d6cd7d
Frederik Dudzik <frederik@dudzik.co>
2016-07-30 21:19:14 +0200

    add find_with_gitignore

---
+++ find_with_gitignore.rb
@@ -0,0 +1,12 @@
+class FindWithGitignore < Formula
+  desc ""
+  homepage "https://github.com/doodzik/find_with_gitignore"
+  url "https://github.com/doodzik/find_with_gitignore/archive/0.0.1.tar.gz"
+  version "0.0.1"
+  sha256 "7df99254d4c11c4368ab4dee31a8d43900cb1df16d78d9b77cf922a5d4dee476"
+
+  def install
+    bin.install "find_with_gitignore"
+  end
+end
+
